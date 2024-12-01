d53765a3a2639cd8445f0a1d30e58c23f7287c1e
Nick Jacobs <nick@highlandcows.com>
2024-12-01 06:56:23 +0200

    chore: Code clean-up

--- sorting-algorithms/src/lib.rs
+++ sorting-algorithms/src/lib.rs
@@ -1,8 +1,8 @@
 // Insertion sort, p. 17
 pub fn insertion_sort(array: &mut [i32]) {
-    for j  in 1..array.len() {
-        let key = array[j];
-        let mut i = (j - 1) as i32;
+    for j  in 2..array.len() {
+        let key: i32 = array[j];
+        let mut i: i32 = (j - 1) as i32;
         while i >= 0 && array[i as usize] > key {
             array[(i + 1) as usize] = array[i as usize];
             i -= 1;
--- sorting-algorithms/src/main.rs
+++ sorting-algorithms/src/main.rs
@@ -1,9 +1,18 @@
-use rand::distr::StandardUniform;
-use rand::Rng;
+use rand::prelude::ThreadRng;
+use rand::seq::SliceRandom;
 use sorting_algorithms::insertion_sort;
 
 fn main() {
-    let mut vec: Vec<i32> = rand::rng().sample_iter(StandardUniform).take(10).collect();
-    insertion_sort(vec.as_mut_slice());
-    println!("{:?}", vec);
+    let mut numbers: Vec<i32> = random_numbers(100);
+    println!("numbers = {:?}", numbers);
+    insertion_sort(numbers.as_mut_slice());
+    println!("sorted numbers = {:?}", numbers);
+}
+
+fn random_numbers(num: i32) -> Vec<i32> {
+    let mut vec: Vec<i32> = (0..num).collect();
+    let mut rng: ThreadRng = rand::rng();
+    vec.shuffle(&mut rng);
+    
+    vec 
 }
