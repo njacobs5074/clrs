e7b1b5be00d89028b15729056e273af4e2c4c1ff
Nick Jacobs <nick@highlandcows.com>
2024-12-03 07:43:45 +0200

    fix: Fixed off by 1 bug in sorting and changed size of numbers to be 20

--- sorting-algorithms/src/lib.rs
+++ sorting-algorithms/src/lib.rs
@@ -1,6 +1,6 @@
 // Insertion sort, p. 17
 pub fn insertion_sort(array: &mut [i32]) {
-    for j  in 2..array.len() {
+    for j in 1..array.len() {
         let key: i32 = array[j];
         let mut i: i32 = (j - 1) as i32;
         while i >= 0 && array[i as usize] > key {
--- sorting-algorithms/src/main.rs
+++ sorting-algorithms/src/main.rs
@@ -3,7 +3,7 @@ use rand::seq::SliceRandom;
 use sorting_algorithms::insertion_sort;
 
 fn main() {
-    let mut numbers: Vec<i32> = random_numbers(100);
+    let mut numbers: Vec<i32> = random_numbers(20);
     println!("numbers = {:?}", numbers);
     insertion_sort(numbers.as_mut_slice());
     println!("sorted numbers = {:?}", numbers);
@@ -13,6 +13,6 @@ fn random_numbers(num: i32) -> Vec<i32> {
     let mut vec: Vec<i32> = (0..num).collect();
     let mut rng: ThreadRng = rand::rng();
     vec.shuffle(&mut rng);
-    
-    vec 
+
+    vec
 }
