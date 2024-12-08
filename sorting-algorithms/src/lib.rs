3a772bfa3a7c1396f9004200f113d7cceb91d162
Nick Jacobs <nick@highlandcows.com>
2024-12-08 16:24:24 +0200

    feat: Genericized insertion sort that works for anything that implements PartialOrd and Clone

--- sorting-algorithms/src/lib.rs
+++ sorting-algorithms/src/lib.rs
@@ -1,10 +1,10 @@
 // Insertion sort, p. 17
-pub fn insertion_sort(array: &mut [i32]) {
+pub fn insertion_sort<T: PartialOrd + Clone>(array: &mut [T]) {
     for j in 1..array.len() {
-        let key: i32 = array[j];
+        let key = array[j].clone();
         let mut i: i32 = (j - 1) as i32;
         while i >= 0 && array[i as usize] > key {
-            array[(i + 1) as usize] = array[i as usize];
+            array.swap((i + 1) as usize, i as usize);
             i -= 1;
         }
         array[(i + 1) as usize] = key;
--- sorting-algorithms/src/main.rs
+++ sorting-algorithms/src/main.rs
@@ -3,14 +3,31 @@ use rand::seq::SliceRandom;
 use sorting_algorithms::insertion_sort;
 
 fn main() {
-    let mut numbers: Vec<i32> = random_numbers(20);
+    let mut numbers: Vec<i32> = random_integers(20);
+    println!("numbers = {:?}", numbers);
+    insertion_sort(numbers.as_mut_slice());
+    println!("sorted numbers = {:?}", numbers);
+
+    let mut numbers: Vec<f32> = random_floats(20);
     println!("numbers = {:?}", numbers);
     insertion_sort(numbers.as_mut_slice());
     println!("sorted numbers = {:?}", numbers);
 }
 
-fn random_numbers(num: i32) -> Vec<i32> {
+fn random_integers(num: i32) -> Vec<i32> {
     let mut vec: Vec<i32> = (0..num).collect();
+    let mut rng: ThreadRng = rand::rng();
+    vec.shuffle(&mut rng);
+
+    vec
+}
+
+fn random_floats(num: i32) -> Vec<f32> {
+    let mut vec: Vec<f32> = Vec::with_capacity(num as usize);
+    for i in 0..num {
+        vec.push(i as f32);
+    }
+
     let mut rng: ThreadRng = rand::rng();
     vec.shuffle(&mut rng);
 
