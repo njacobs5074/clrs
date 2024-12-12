68303578e83321578a1a7ec4778384739765d224
Nick Jacobs <nick@highlandcows.com>
2024-12-12 21:40:34 +0200

    feat: Added merge sort courtesy of https://mohitkarekar.com/posts/2020/merge-sort-in-rust/

--- sorting-algorithms/src/lib.rs
+++ sorting-algorithms/src/lib.rs
@@ -10,3 +10,57 @@ pub fn insertion_sort<T: PartialOrd + Clone>(array: &mut [T]) {
         array[(i + 1) as usize] = key;
     }
 }
+
+// From https://mohitkarekar.com/posts/2020/merge-sort-in-rust/
+fn merge<T: PartialOrd + Clone>(left: &Vec<T>, right: &Vec<T>) -> Vec<T> {
+    let mut i = 0;
+    let mut j = 0;
+    let mut merged: Vec<T> = Vec::new();
+
+    // Put the elements of left & right into the correct place in merged
+    while i < left.len() && j < right.len() {
+        if left[i] < right[j] {
+            merged.push(left[i].clone());
+            i += 1;
+        } else {
+            merged.push(right[j].clone());
+            j += 1;
+        }
+    }
+
+    // There might be some leftovers in the left vector - just copy them in to merged
+    if i < left.len() {
+        while i < left.len() {
+            merged.push(left[i].clone());
+            i += 1;
+        }
+    }
+
+    // Ditto for the leftovers in the right vector
+    if j < right.len() {
+        while j < right.len() {
+            merged.push(right[j].clone());
+            j += 1;
+        }
+    }
+
+    merged
+}
+
+// From https://mohitkarekar.com/posts/2020/merge-sort-in-rust/
+pub fn merge_sort<T: PartialOrd + Clone>(vec: &Vec<T>) -> Vec<T> {
+    if vec.len() < 2 {
+        // Trivially sorted
+        vec.to_vec()
+    } else {
+        // Otherwise we must divide & conquer, i.e., merge sort the
+        // left hand vector and then merge sort the right hand vector
+        let middle = vec.len() / 2;
+        let left = merge_sort(&vec[0..middle].to_vec());
+        let right = merge_sort(&vec[middle..].to_vec());
+
+        // The left and right are sorted so now we just need to merge them
+        // and return the result
+        merge(&left, &right)
+    }
+}
--- sorting-algorithms/src/main.rs
+++ sorting-algorithms/src/main.rs
@@ -1,8 +1,17 @@
 use rand::prelude::ThreadRng;
 use rand::seq::SliceRandom;
-use sorting_algorithms::insertion_sort;
+use sorting_algorithms::{insertion_sort, merge_sort};
+use std::f32::consts::PI;
 
 fn main() {
+    println!("Insertion sort");
+    run_insertion_sort();
+
+    println!("Merge sort");
+    run_merge();
+}
+
+fn run_insertion_sort() {
     let mut numbers: Vec<i32> = random_integers(20);
     println!("numbers = {:?}", numbers);
     insertion_sort(numbers.as_mut_slice());
@@ -12,6 +21,19 @@ fn main() {
     println!("numbers = {:?}", numbers);
     insertion_sort(numbers.as_mut_slice());
     println!("sorted numbers = {:?}", numbers);
+}
+
+fn run_merge() {
+    let numbers = random_integers(20);
+
+    println!("numbers = {:?}", numbers);
+    let sorted_numbers = merge_sort(&numbers);
+    println!("sorted_numbers = {:?}", sorted_numbers);
+
+    let numbers = random_floats(20);
+    println!("numbers = {:?}", numbers);
+    let sorted_numbers = merge_sort(&numbers);
+    println!("sorted_numbers = {:?}", sorted_numbers);
 }
 
 fn random_integers(num: i32) -> Vec<i32> {
@@ -25,7 +47,7 @@ fn random_integers(num: i32) -> Vec<i32> {
 fn random_floats(num: i32) -> Vec<f32> {
     let mut vec: Vec<f32> = Vec::with_capacity(num as usize);
     for i in 0..num {
-        vec.push(i as f32);
+        vec.push(i as f32 * PI);
     }
 
     let mut rng: ThreadRng = rand::rng();
